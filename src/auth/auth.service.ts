7b202d9de2c8250d0a9be3bd49865540fc2587f8
gideonoludeyi <gooludeyi@gmail.com>
2022-05-23 20:55:31 -0400

    fix(auth): validate email

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -5,9 +5,18 @@ import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class AuthService {
+  /**
+   * Regex obtained from {@link https://stackoverflow.com/a/201378 StackOverflow}
+   */
+  private emailRegex =
+    /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
+
   constructor(private prisma: PrismaService) {}
 
   async signUp(email: string, password: string) {
+    const isValidEmail = this.emailRegex.test(email);
+    if (!isValidEmail) throw new Error('Invalid email');
+
     const account = await this.prisma.account.findUnique({
       where: { email },
     });
@@ -33,6 +42,9 @@ export class AuthService {
   }
 
   async login(email: string, password: string) {
+    const isValidEmail = this.emailRegex.test(email);
+    if (!isValidEmail) throw new Error('Invalid email');
+
     const account = await this.prisma.account.findUnique({
       where: { email },
     });
