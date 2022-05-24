bb71df885bfadddea730d1b080ac336ceb98d17b
gideonoludeyi <gooludeyi@gmail.com>
2022-05-23 21:16:14 -0400

    feat(auth): implement signup and login routes

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,52 @@
-import { Controller } from '@nestjs/common';
+import {
+  BadRequestException,
+  Body,
+  ConsoleLogger,
+  Controller,
+  Post,
+} from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  private logger = new ConsoleLogger(this.constructor.name);
+
+  constructor(private service: AuthService, private jwt: JwtService) {}
+
+  @Post('signup')
+  async signUp(
+    @Body('email') email: string,
+    @Body('password') password: string,
+  ) {
+    try {
+      const account = await this.service.signUp(email, password);
+      return account;
+    } catch (error) {
+      this.logger.error(error);
+      throw new BadRequestException();
+    }
+  }
+
+  @Post('login')
+  async login(
+    @Body('email') email: string,
+    @Body('password') password: string,
+  ) {
+    let account: any;
+    try {
+      account = await this.service.login(email, password);
+    } catch (error) {
+      this.logger.error(error);
+      throw new BadRequestException();
+    }
+
+    const token = this.jwt.sign({
+      sub: account.id,
+    });
+
+    return {
+      access_token: token,
+    };
+  }
+}
