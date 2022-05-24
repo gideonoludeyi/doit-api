f5c3a17e9a44eb1ca4a24f242debd182be3f5812
gideonoludeyi <gooludeyi@gmail.com>
2022-05-23 21:22:07 -0400

    feat: configure access to environment variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,10 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
 @Module({
-  imports: [AuthModule],
+  imports: [
+    AuthModule,
+    ConfigModule.forRoot({ isGlobal: true, envFilePath: '.env' }),
+  ],
 })
 export class AppModule {}
