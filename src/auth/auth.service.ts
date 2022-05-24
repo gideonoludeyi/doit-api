c13a483b686454c5f471b930e7758b898f64740b
gideonoludeyi <gooludeyi@gmail.com>
2022-05-23 20:48:11 -0400

    feat(auth): implement account registration

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,28 @@
 import { Injectable } from '@nestjs/common';
+import { hashSync } from 'bcryptjs';
+import { nanoid } from 'nanoid';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private prisma: PrismaService) {}
+
+  async signUp(email: string, password: string) {
+    const account = await this.prisma.account.findUnique({
+      where: { email },
+    });
+
+    if (!account) throw new Error('Email already registered');
+
+    const id = nanoid();
+    const hashedPassword = hashSync(password);
+
+    return await this.prisma.account.create({
+      data: {
+        id,
+        email,
+        password: hashedPassword,
+      },
+    });
+  }
+}
